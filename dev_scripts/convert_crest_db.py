aee2ffe6832e983379d48d9889744f1b6551a8c9
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-07-13 19:50:11 +0200

    Update script to automatically transform, compress and upload the BOLD database files

--- dev_scripts/convert_crest_db.py
+++ dev_scripts/convert_crest_db.py
@@ -9,7 +9,7 @@ format used in `crest4`.
 """
 
 # Imports #
-import inspect
+import os, inspect, sh
 from autopaths import Path
 from fasta import FASTA
 
@@ -63,6 +63,10 @@ class OldDatabase:
     def new_fasta(self):
         return FASTA(self.new_dir + self.short_name + '.fasta')
 
+    @property
+    def new_tar_gz(self):
+        return self.new_dir.directory + self.short_name + '.tar.gz'
+
     #------------------------------- Conversion ------------------------------#
     def convert_map(self):
         # How to convert the lines #
@@ -70,7 +74,9 @@ class OldDatabase:
             result = []
             for line in lines:
                 num, name, minus, frac = line.strip().split('\t')
-                if ',' in name: raise Exception("Got a comma in the name.")
+                if ',' in name:
+                    print("Removed a comma from '%s'" % name)
+                    name = name.replace(',', '')
                 if frac != '-1': continue
                 result.append((num, name))
             result.sort(key=lambda elem: int(elem[0]))
@@ -100,15 +106,40 @@ class OldDatabase:
         return self.new_names
 
     def convert_fasta(self):
+        # If the FASTA is not present, copy it from the original #
+        if not self.new_fasta:
+            print("Duplicated the FASTA file to '%s'" % self.new_fasta)
+            self.orig_fasta.copy(self.new_fasta)
         # Remove duplicate entries #
         self.new_fasta.remove_duplicates()
         # Back transcribe #
         self.new_fasta.convert_U_to_T()
 
     def convert(self):
+        # Create a directory if it doesn't exist #
+        self.new_dir.create_if_not_exists()
+        # Call methods #
         self.convert_map()
         self.convert_names()
         self.convert_fasta()
+        # Copy the tree file #
+        self.orig_tre.copy(self.new_tre)
+        # Prepare to compress the directory #
+        print("Compressing the directory at '%s'" % self.new_dir)
+        env_vars = os.environ.copy()
+        env_vars['COPYFILE_DISABLE'] = '1'
+        options = ["--no-mac-metadata", "--exclude", ".DS_Store",
+                   "--options", "gzip:compression-level=9", "-zcvf"]
+        # Compress the directory #
+        os.chdir(self.new_dir.directory)
+        sh.tar(*options, self.new_tar_gz, self.short_name,
+               _env=env_vars, _fg=True)
+        # Upload to the AWS S3 bucket #
+        import boto3
+        s3 = boto3.resource('s3')
+        bucket = s3.Bucket('crest4')
+        response = bucket.upload_file(self.short_name, self.short_name)
+        assert response['ResponseMetadata']['HTTPStatusCode'] == 200
 
 ###############################################################################
 class SilvaMod128(OldDatabase):
@@ -128,11 +159,21 @@ class SilvaMod138(OldDatabase):
     # The real name #
     short_name = 'silvamod138'
 
+#-----------------------------------------------------------------------------#
+class Bold(OldDatabase):
+    """
+    Represents the new bold database.
+    """
+
+    # The real name #
+    short_name = 'bold'
+
 ###############################################################################
 # As our databases should only be converted on disk once, we have singletons #
 silvamod128 = SilvaMod128()
 silvamod138 = SilvaMod138()
+bold = Bold()
 
 # How to use these objects #
 if __name__ == '__main__':
-    silvamod138.convert()
+    bold.convert()
